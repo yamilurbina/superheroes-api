83840f5015a8187add301c08c6425e672cb06095
Yamil Urbina <hello@yamilurbina.com>
2021-03-27 16:43:33 -0600

    Adding more tests

--- test/test.js
+++ test/test.js
@@ -38,5 +38,32 @@ describe("Superheroes", function() {
       }
       done()
     })
+
+    it("Should get one superhero", (done) => {
+      chai.request(server)
+        .get("/superheroes/batman")
+        .end((err, result) => {
+          result.should.have.status(200);
+          done()
+        })
+    })
+
+    it("Should update a single superhero", (done) => {
+      var updateSuperhero = {
+        "slug": "batman",
+        "name": "Batman",
+        "year": 1940,
+        "author": "Bob Kane"
+      }
+
+      chai.request(server)
+        .put("/superheroes/batman")
+        .send(updateSuperhero)
+        .end((err, result) => {
+          result.should.have.status(200);
+          done()
+        })
+    })
+
   })
 })
